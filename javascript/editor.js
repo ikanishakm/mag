e18baaf901f0b376cd22c6b3534411bf8388323b
i.kanishakm <i.kanishakm@gmail.com>
2023-08-14 17:49:41 +0530

    inital commit

---
+++ javascript/editor.js
@@ -0,0 +1,112 @@
+var pageEditor = {
+  setting: {},
+  pageAnnos: [
+    [],
+    [],
+    [
+      {
+        annotype: "com.mobiano.flipbook.pageeditor.TAnnoLink",
+        location: {
+          x: "0.431373",
+          y: "0.917989",
+          width: "0.133987",
+          height: "0.011905",
+        },
+        action: {
+          triggerEventType: "mouseDown",
+          actionType: "com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL",
+          url: "http://fisherpaykel.com",
+          linkTarget: "_blank",
+        },
+      },
+      {
+        annotype: "com.mobiano.flipbook.pageeditor.TAnnoLink",
+        location: {
+          x: "0.508170",
+          y: "0.572751",
+          width: "0.104575",
+          height: "0.011905",
+        },
+        action: {
+          triggerEventType: "mouseDown",
+          actionType: "com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL",
+          url: "http://fisherpaykel.com",
+          linkTarget: "_blank",
+        },
+      },
+    ],
+    [],
+    [],
+    [],
+    [
+      {
+        annotype: "com.mobiano.flipbook.pageeditor.TAnnoLink",
+        location: {
+          x: "0.728758",
+          y: "0.593915",
+          width: "0.078431",
+          height: "0.013228",
+        },
+        action: {
+          triggerEventType: "mouseDown",
+          actionType: "com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL",
+          url: "http://sharp.ca",
+          linkTarget: "_blank",
+        },
+      },
+    ],
+    [
+      {
+        annotype: "com.mobiano.flipbook.pageeditor.TAnnoLink",
+        location: {
+          x: "0.187909",
+          y: "0.936508",
+          width: "0.132353",
+          height: "0.013228",
+        },
+        action: {
+          triggerEventType: "mouseDown",
+          actionType: "com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL",
+          url: "http://Caesarstone.ca",
+          linkTarget: "_blank",
+        },
+      },
+    ],
+    [],
+    [
+      {
+        annotype: "com.mobiano.flipbook.pageeditor.TAnnoLink",
+        location: {
+          x: "0.424837",
+          y: "0.781746",
+          width: "0.151961",
+          height: "0.013228",
+        },
+        action: {
+          triggerEventType: "mouseDown",
+          actionType: "com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL",
+          url: "http://houseandhome.com",
+          linkTarget: "_blank",
+        },
+      },
+    ],
+    [
+      {
+        annotype: "com.mobiano.flipbook.pageeditor.TAnnoLink",
+        location: {
+          x: "0.057190",
+          y: "0.046296",
+          width: "0.080065",
+          height: "0.010582",
+        },
+        action: {
+          triggerEventType: "mouseDown",
+          actionType: "com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL",
+          url: "http://KOHLER.ca",
+          linkTarget: "_blank",
+        },
+      },
+    ],
+    [],
+  ],
+}
